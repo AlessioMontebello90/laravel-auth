b4248806667e85ff106cc9032d5923fbe2d0d826
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-10-25 18:30:02 +0200

    fix layout form new project

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -7,24 +7,28 @@
 @endsection
 
 @section('content')
-    <h1 class="container my-1">create</h1>
+    <h1 class="container my-1">Add new project</h1>
     <div class="container mt-5">
         <a class="" href="{{ route('admin.projects.index') }}">
             <div class="my-3 btn btn-success">
                 back to portfolio list
             </div>
         </a>
-        <table class="table">
+        <section class="debug">
+            <form action="{{ route('admin.projects.store') }}" method="POST">
+                @csrf
 
-            <tbody>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Slug</th>
-                <th scope="col">Url</th>
-                <th scope="col">Description</th>
-                <th scope="col" colspan="3">Action</th>
-            </tbody>
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" />
 
-        </table>
+                <label for="Url" class="form-label">Url</label>
+                <textarea class="form-control" id="Url" name="Url" rows="1"></textarea>
+
+                <label for="Description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="5"></textarea>
+
+                <button type="submit" class="btn btn-warning mt-5">Save</button>
+            </form>
     </div>
+    </section>
 @endsection
